c349f63a499cf7cf79af99c00bbe39e91c0592e5
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2023-09-16 00:42:01 +0800

    users added

---
+++ fakeData.js
@@ -0,0 +1,53 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John Doe",
+    username: "johndoe",
+    age: 30,
+    nationality: "United States",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    username: "janesmith",
+    age: 25,
+    nationality: "Canada",
+  },
+  {
+    id: 3,
+    name: "David Johnson",
+    username: "davidj",
+    age: 35,
+    nationality: "United Kingdom",
+  },
+  {
+    id: 4,
+    name: "Emily Brown",
+    username: "emilyb",
+    age: 28,
+    nationality: "Australia",
+  },
+  {
+    id: 5,
+    name: "Michael Lee",
+    username: "michaellee",
+    age: 32,
+    nationality: "South Korea",
+  },
+  {
+    id: 6,
+    name: "Sophia Martinez",
+    username: "sophiam",
+    age: 29,
+    nationality: "Mexico",
+  },
+  {
+    id: 7,
+    name: "Daniel Nguyen",
+    username: "danieln",
+    age: 31,
+    nationality: "Vietnam",
+  },
+];
+
+module.exports={UserList};
