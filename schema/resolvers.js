9ba712f9d0b25f60c1449ada09a496d609cd3c80
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2023-09-16 17:28:18 +0800

    delete user by id

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -54,6 +54,13 @@ const resolvers = {
             });
             return userUpdated;
         },
+
+        deleteUser: (parent,args)=> {
+            const id= args.id;
+            _.remove(UserList, (user)=> user.id === Number(id));
+            return null;
+            // return `User with ${id} deleted`;
+        },
     },
   };
 
